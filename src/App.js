7cbe60de5f1516fe0261607dc1bfbe8db66f1594
NHammond <natehammond62@gmail.com>
2024-07-12 18:13:42 -0400

    casing was messed up between imports, this is half of the full correction

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Briefing from './pages/Briefing';
-import CounterDynamical from './pages/CounterDynamical';
-import ExoticTech from './pages/ExoticTech';
-import HomePage from './pages/HomePage';
+import CounterDynamical from '/pages/CounterDynamical';
+import ExoticTech from '/pages/ExoticTech';
+import HomePage from '/pages/HomePage';
 
 
 function App() {
